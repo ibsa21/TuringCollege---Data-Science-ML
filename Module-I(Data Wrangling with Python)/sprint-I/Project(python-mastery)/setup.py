3c317951f2f4cd8b2bbffecb165d0dfdd14c35c0
ibsa21 <ibsaabraham663@gmail.com>
2022-07-04 14:27:40 +0300

    python Arithmetic Calculator Package

---
+++ Module-I(Data Wrangling with Python)/sprint-I/Project(python-mastery)/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='arithmetic_calculator',
+    version='0.1',
+    description='Arithmetic Calculator',
+    author='Ibsa Abraham',
+    packages=['arithmetic_calculator'],
+    zip_safe = False
+)
